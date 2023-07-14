a305233c87293fcb4bf3227ba96aba94d1e082d6
Alyzee Sosa <alyzeesosacollege@gmail.com>
2023-07-14 13:16:24 -0700

    Initial Commit

---
+++ NavigationPractice/NavigationPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationPracticeApp.swift
+//  NavigationPractice
+//
+//  Created by Scholar on 7/14/23.
+//
+
+import SwiftUI
+
+@main
+struct NavigationPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
