2706fa203e89611c5b6ec9e490e7766bc4e9d8c5
Alyzee Sosa <alyzeesosacollege@gmail.com>
2023-07-20 14:29:11 -0700

    commit

--- NavigationPractice/ContentView.swift
+++ NavigationPractice/ContentView.swift
@@ -8,14 +8,35 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationStack {
+            
+            // Root view - the first and main page users see when entering the application
+            // subviews = elements
+            VStack (spacing: 20){
+                Text("Root view").font(.system(size: 45)).fontWeight(.heavy)
+                
+                // links to navigate to different
+                
+                // get directly from library
+                
+                // to link another page, label the destination as that page's name and add function ()
+                NavigationLink(destination: SecondView()) {
+                    Text("Click here!")
+                }.font(.system(size: 30))
+                
+//                NavigationLink(destination:Text("Second View, Page 2")){
+//                    Text("Click here also!")
+//               }
+                
+            }
+            
+            
         }
-        .padding()
+        
     }
 }
 
---
+++ NavigationPractice/SecondView.swift
@@ -0,0 +1,22 @@
+//
+//  SecondView.swift
+//  NavigationPractice
+//
+//  Created by Scholar on 7/14/23.
+//
+
+// create with file --> new file --> swift view to get base code
+
+import SwiftUI
+
+struct SecondView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SecondView_Previews: PreviewProvider {
+    static var previews: some View {
+        SecondView()
+    }
+}
